ebb5261730eb53e14ee1eccf8324cdcc47c1abc2
Julita Bielik <julz@o2.pl>
2014-09-21 12:45:58 +0200

    test
    test

--- include/Window.h
+++ include/Window.h
@@ -10,14 +10,14 @@ using namespace std;
 class Window
 {
 	bool success;
-	
+
 	SDL_Rect cameraLocation;//the x and y params is where you should start rendering from the canvas
 	SDL_Rect sceneSize;//the h and w params are the size of the screen
 	SDL_Window* window;// the window of this window
 	SDL_Renderer* renderer = NULL;//the rnderer
 	Texture canvas;//the canvas
-	int winWidth = 400;//the initial width of the window
-	int winHeight = 400;//the initial height of the window
+	int winWidth = 256;//the initial width of the window
+	int winHeight = 240;//the initial height of the window
 	public:
 		bool resized;
 		Window();
@@ -30,7 +30,7 @@ class Window
 		~Window();
 	protected:
 	private:
-		
+
 };
 
 #endif // WINDOW_H
