c1a32a8cf0896b9a43dc1f7ac970f3007c718ca1
Julita Bielik <julz@o2.pl>
2014-10-05 10:39:44 +0200

    First real little changes!
    Added some attributes to character class

--- RetardedGame/Character.h
+++ RetardedGame/Character.h
@@ -8,13 +8,14 @@ using namespace std;
 
 class Character
 {
-	
 	public:
 		void render();
 		void setTexturePointer(Texture* _texture);
 		virtual void move();
 		virtual ~Character();
 	protected:
+		string name;//name of the character
+		int hearts;//amount of hears
 		Texture* texture;//the pointer to the sprite sheet
 		SDL_Rect spriteSheetLoc;//where to find the sprite on the sheet 
 		SDL_Rect worldLoc;//where the character is in the world
